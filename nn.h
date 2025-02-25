7e6bc6aa6eab0a716bc21f96fd0b555f01b50d6b
Pranjwal-Jha <pranjwaljha2005@gmail.com>
2025-02-25 16:26:06 +0530

    Added forward function and construstor for multiple hidden class

--- nn.h
+++ nn.h
@@ -1,25 +1,38 @@
-#ifndef NN_H
+ #ifndef NN_H
 #define NN_H
 #include <vector>
 #include "matrix.h"
 #include "activation.h"
 class NeuralNetwork{
 private:
-    // std::vector<size_t> topology; // to use the input size, hidden size and output size
-    Matrix weights;
-    Matrix bias;
+    std::vector<size_t> topology; // input layer feature(cols), hidden layer , output layer <- input in this form
+    std::vector<Matrix> weights;
+    std::vector<Matrix> bias;
 public:
-    NeuralNetwork(size_t inputsize)
-        : weights(1,inputsize),bias(1,1){
-            weights.randomize();
-            bias.randomize();
+    NeuralNetwork(const std::vector<size_t>& inputsize)
+        : topology(inputsize){
+            if(inputsize.size()<2) throw std::invalid_argument("Topology must have atleast an input and output layer");
+            for(size_t i=1;i<topology.size();i++){
+                weights.push_back(Matrix(topology[i-1],topology[i]));
+                weights.back().randomize();
+                bias.push_back(Matrix(topology[i],1));
+                bias.back().randomize();
+            }
         }
+
+
     Matrix forward(const Matrix& input){
-        Matrix output = input*weights.Transpose();
-        for(size_t rows=0;rows<output.GetRow();rows++){
-            output.at(rows,0)+=bias.at(0,0);
+        Matrix curr_output = input;
+        for(size_t i=0;i<weights.size();i++){
+            Matrix layer_output = curr_output*weights[i];
+            for(size_t row=0;row<layer_output.GetRow();row++){
+                for(size_t col=0;col<layer_output.GetCol();col++){
+                    layer_output.at(row,col)+=bias[i].at(col,0);
+                }
+            }
+            curr_output = Activation::sigmoid(layer_output);
         }
-        return Activation::sigmoid(output);
+        return curr_output;
     }
 };
 #endif
