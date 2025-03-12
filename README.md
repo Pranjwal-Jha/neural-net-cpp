83b6a96ce4d53ec78d2d4efe5b3597c9ec936a03
Pranjwal-Jha <pranjwaljha2005@gmail.com>
2025-03-12 18:53:53 +0530

    Final Release Version

--- README.md
+++
@@ -1,2 +0,0 @@
-<h1>Neural Network from Scratch</h1>
-    <p>A minimal neural network implemented in C++ without external deep learning libraries.</p>
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <iostream>
-using namespace std;
-int main(){
-    vector<vector<int>> v = {{1,2,3,4,4,5,},{3,4,5,5,56}};
-    std::cout << v.size();
-    std::cout << std::endl;
-    return 0;
-}
